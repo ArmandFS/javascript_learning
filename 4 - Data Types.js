748937f2cbb8430a4cd132e3f45cb7a6cc9cf018
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-12 23:16:59 +0700

    Add files via upload

---
+++ 4 - Data Types.js
@@ -0,0 +1,22 @@
+
+let age = 23;
+let firstName = 'Jonas';
+let fullAge = true;
+let children;
+
+//typeof usage
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun)
+//assign new value in existing variable
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun)
+
+//empty value examples
+let year;
+console.log(year);
+console.log(typeof year);
