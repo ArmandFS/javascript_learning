91974a82fb93da24bd0db83fca255622d7ac26c7
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-18 20:21:58 +0700

    Add files via upload

---
+++ 5 - let const var.js
@@ -0,0 +1,19 @@
+//example of reasignin a value in js
+//or mutation
+//recap we can change from this true
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//into
+javascriptisFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+//const usage
+const birthYear = 1999;
+birthYear = 1990
+//this doesn't work.
+
+//var usage
+var job = 'programmer';
+job = 'teacher';
+
