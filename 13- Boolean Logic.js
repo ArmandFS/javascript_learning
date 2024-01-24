78f5f0235d7de7a1a01824d6ddc420d9373a567d
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-24 22:43:18 +0700

    Update 13- Boolean Logic.js (added and or operators examples)

---
+++ 13- Boolean Logic.js
@@ -0,0 +1,35 @@
+//AND operator example
+let x = 5;
+let y = 10;
+if (x > 0 && y > 0) {
+    console.log("Both x and y are greater than 0");
+} else {
+    console.log("At least one of x or y is not greater than 0");
+}
+
+//Example 2
+let isSunShining = true;
+let isWarm = true;
+if (isSunShining && isWarm) {
+    console.log("It's a sunny and warm day!");
+} else {
+    console.log("It's not sunny and warm.");
+}
+
+//OR operator example
+// Example 1
+let age = 25;
+if (age < 18 || age >= 60) {
+    console.log("You are either below 18 or 60 and above.");
+} else {
+    console.log("You are between 18 and 60.");
+}
+
+// Example 2
+let isRaining = true;
+let hasUmbrella = false;
+if (isRaining || hasUmbrella) {
+    console.log("You won't get wet!");
+} else {
+    console.log("You might get wet.");
+}
