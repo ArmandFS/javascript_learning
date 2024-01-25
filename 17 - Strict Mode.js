45905f8f05764a14a434562fbeea72dbc0900ff8
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-25 22:39:02 +0700

    Update 17 - Strict Mode.js

---
+++ 17 - Strict Mode.js
@@ -0,0 +1,15 @@
+//activate strict mode
+'use strict';
+//write more secure code and avoid accidental errors.
+
+let hasDriversLicense = false;
+const passTest = true;
+
+//write logic for person that passed drivers test
+//with wrong variable name on purpose
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive.')
+
+//strict mode reserve variable
+const interface = 'Audio';
+const private = 30;
