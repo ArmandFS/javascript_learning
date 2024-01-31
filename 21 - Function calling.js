86ec284db1e55173b5847a54e3a8aebf571fb495
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-31 11:48:39 +0700

    Update 21 - Function calling.js

---
+++ 21 - Function calling.js
@@ -0,0 +1,14 @@
+//functions calling other functions
+//make a fruitprocessor that needs smaller bits of fruits, which requires another function
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
+    return juice;
+}
+
+console.log(fruitProcessor(2,3));
