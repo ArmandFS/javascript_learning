7b6337795b1d054794af93cdd796f6a9b96710d4
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-01-31 13:28:00 +0700

    Add files via upload

---
+++ Challenge_5.js
@@ -0,0 +1,7 @@
+//challenge 5
+//Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+//A team only wins if it has at least double the average score of the other team. Otherwise, no team wins
+
+//task 1:
+//create an arrow function calcAverage to calculate average of 3 scores. Function has 3 parameters
