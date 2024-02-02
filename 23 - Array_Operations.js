b02a746107850b444e3da38029f18ba971ed138f
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-02 22:34:22 +0700

    Update 23 - Array_Operations.js

---
+++ 23 - Array_Operations.js
@@ -0,0 +1,20 @@
+//array methods and operations
+const friends = ['Michael', 'Steven', 'Peter'];
+//push method in javascript (a function)
+friends.push('Jay');
+
+//other methods
+//add element in first element of array
+friends.unshift('John');
+
+//remove last element from an array (no argument passed)
+friends.pop();
+
+//remove first element in array
+friends.shift();
+
+//position of index of element
+friends.indexOf('Michael');
+
+//method to tell if element is in array or not
+friends.includes('Steven'); //result is true
