bca4d553a4c6ba3bbd402f84b6923d8df2c0a89f
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-02-05 12:00:24 +0700

    Update 24 - Object_intro.js

---
+++ 24 - Object_intro.js
@@ -0,0 +1,17 @@
+//example of an Object in JavaScript
+const jonas = {
+   firstName: 'Jonas',
+   lastName: 'Schmedtmann',
+   age: 2037 - 1991,
+   job: 'programmer',
+   friends: ['Michael', 'Peter','Steven']
+
+
+//another example using my name :D
+const Armand = {
+    firstName: 'Armand',
+    lastName: 'Surbakti',
+    age: 21;
+    job: 'Machine Learning Engineer',
+    friends: ['Jason', 'Alex', 'Virgie']
+}
